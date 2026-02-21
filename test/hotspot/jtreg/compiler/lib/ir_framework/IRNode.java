dd0072536aafdbde0421abc4c3ad8c7294c5fa89
Yasumasa Suenaga <yasuenag@gmail.com>
2026-02-21 16:49:01 +0900

    Add test

--- test/hotspot/jtreg/compiler/lib/ir_framework/IRNode.java
+++ test/hotspot/jtreg/compiler/lib/ir_framework/IRNode.java
@@ -1478,6 +1478,11 @@ public class IRNode {
         beforeMatchingNameRegex(VECTOR_MASK_FIRST_TRUE, "VectorMaskFirstTrue");
     }
 
+    public static final String CALL_LEAF_VECTOR = PREFIX + "CALL_LEAF_VECTOR" + POSTFIX;
+    static {
+        beforeMatchingNameRegex(CALL_LEAF_VECTOR, "CallLeafVector");
+    }
+
     // Can only be used if avx512_vnni is available.
     public static final String MUL_ADD_VS2VI_VNNI = PREFIX + "MUL_ADD_VS2VI_VNNI" + POSTFIX;
     static {
